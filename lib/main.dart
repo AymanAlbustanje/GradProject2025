784f5005c6f768c6471789c010c21d00b53b565e
AymanAlbustanje <201125@ppu.edu.ps>
2025-04-28 02:22:27 +0300

    improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gradproject2025/presentation/screens/notifications_screen.dart';
 import 'presentation/screens/home_screen.dart';
 import 'presentation/screens/discover_screen.dart';
 import 'presentation/screens/statistics_screen.dart';
@@ -13,7 +14,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Create a ValueNotifier to manage the theme state
     final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.dark);
 
     return ValueListenableBuilder<ThemeMode>(
@@ -22,46 +22,66 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           title: 'Smart Stock',
           debugShowCheckedModeBanner: false,
-          theme: ThemeData.light().copyWith(
-            primaryColor: const Color(0xFF54ACE3),
-            scaffoldBackgroundColor: Colors.white,
-            appBarTheme: const AppBarTheme(
-              backgroundColor: Color(0xFF54ACE3),
-              foregroundColor: Colors.white,
-              elevation: 0,
-            ),
-            bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-              backgroundColor: Colors.white,
-              selectedItemColor: Color(0xFF54ACE3),
-              unselectedItemColor: Colors.grey,
-            ),
-            textTheme: const TextTheme(
-              bodyLarge: TextStyle(color: Colors.black),
-              bodyMedium: TextStyle(color: Colors.black87),
-            ),
-          ),
-          darkTheme: ThemeData.dark().copyWith(
-            primaryColor: const Color(0xFF1E1E1E),
-            scaffoldBackgroundColor: const Color(0xFF121212),
-            appBarTheme: const AppBarTheme(
-              backgroundColor: Color(0xFF1E1E1E),
-              foregroundColor: Colors.white,
-              elevation: 0,
-            ),
-            bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-              backgroundColor: Color(0xFF1E1E1E),
-              selectedItemColor: Color(0xFF0078D4),
-              unselectedItemColor: Colors.grey,
-            ),
-            textTheme: const TextTheme(
-              bodyLarge: TextStyle(color: Colors.white),
-              bodyMedium: TextStyle(color: Colors.white70),
-            ),
-          ),
-          themeMode: themeMode, // Dynamically switch between light and dark themes
+          theme: _buildLightTheme(),
+          darkTheme: _buildDarkTheme(),
+          themeMode: themeMode,
           home: MainScreen(themeNotifier: themeNotifier),
         );
       },
+    );
+  }
+
+  ThemeData _buildLightTheme() {
+    return ThemeData.light().copyWith(
+      primaryColor: const Color(0xFF54ACE3),
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color(0xFF54ACE3),
+        foregroundColor: Colors.white,
+        elevation: 2,
+        centerTitle: true,
+        toolbarHeight: 52,
+        titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15)),
+        ),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Colors.white,
+        selectedItemColor: Color(0xFF54ACE3),
+        unselectedItemColor: Colors.grey,
+      ),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Colors.black),
+        bodyMedium: TextStyle(color: Colors.black87),
+      ),
+    );
+  }
+
+  ThemeData _buildDarkTheme() {
+    return ThemeData.dark().copyWith(
+      primaryColor: const Color(0xFF1E1E1E),
+      scaffoldBackgroundColor: const Color(0xFF121212),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color(0xFF1E1E1E),
+        foregroundColor: Colors.white,
+        elevation: 2,
+        centerTitle: true,
+        toolbarHeight: 52, // Set the height for all AppBars
+        titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15)),
+        ),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Color(0xFF1E1E1E),
+        selectedItemColor: Color(0xFF0078D4),
+        unselectedItemColor: Colors.grey,
+      ),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Colors.white),
+        bodyMedium: TextStyle(color: Colors.white70),
+      ),
     );
   }
 }
@@ -88,48 +108,57 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('SmartStock'), centerTitle: true),
-      body: _screens[_currentIndex],
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).bottomNavigationBarTheme.backgroundColor, // Dynamic background color
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2), // Subtle shadow
-              spreadRadius: 2,
-              blurRadius: 10,
-              offset: const Offset(0, -2), // Shadow above the bar
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed, // Fixed mode for consistent layout
-            backgroundColor: Theme.of(context).bottomNavigationBarTheme.backgroundColor, // Dynamic background color
-            currentIndex: _currentIndex,
-            selectedItemColor:
-                Theme.of(context).bottomNavigationBarTheme.selectedItemColor, // Dynamic selected item color
-            unselectedItemColor:
-                Theme.of(context).bottomNavigationBarTheme.unselectedItemColor, // Dynamic unselected item color
-            showSelectedLabels: true, // Show labels for selected items
-            showUnselectedLabels: false, // Hide labels for unselected items
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
+      appBar: AppBar(
+        title: const Text('SmartStock'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const NotificationsScreen()));
             },
-            items: const [
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-              BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Discover'),
-              BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Statistics'),
-              BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
-            ],
           ),
+        ],
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: _buildBottomNavigationBar(context),
+    );
+  }
+
+  Widget _buildBottomNavigationBar(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+        borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withAlpha(51), // Equivalent to 20% opacity
+            spreadRadius: 2,
+            blurRadius: 10,
+            offset: const Offset(0, -2),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+          currentIndex: _currentIndex,
+          selectedItemColor: Theme.of(context).bottomNavigationBarTheme.selectedItemColor,
+          unselectedItemColor: Theme.of(context).bottomNavigationBarTheme.unselectedItemColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Discover'),
+            BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Statistics'),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+          ],
         ),
       ),
     );
---
+++ lib/presentation/screens/notifications_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class NotificationsScreen extends StatelessWidget {
+  const NotificationsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notifications'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Navigate back
+          },
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'Notifications Screen',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
